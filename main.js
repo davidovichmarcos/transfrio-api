fbe9d60977ee9f167207d9c2a2579580460cfdf9
davidovich.marcos <davidovich.marcos@gmail.com>
2020-01-28 11:36:00 -0300

    getById endpoints and fixes

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 var bodyParser = require('body-parser')
-
 var express = require('express'),
   app = express(),
   port = parseInt(process.env.PORT, 10) || 2345;
 var firebase = require("firebase/app");
 require('firebase/auth');
 require('firebase/database');
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 var firebaseConfig = require('./firebaseAuth.json');
 
 // Initialize Firebase
@@ -16,14 +15,10 @@ var database = firebase.database();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 
-  app.options('*', (req, res) => {
-      // allowed XHR methods  
+  app.options('*', (req, res) => {// allowed XHR methods  
       res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
       res.send();
   });
@@ -33,9 +28,18 @@ app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
 
-
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to a transfrio api');
+});
+
+app.post('/createUser', (req, res) => {
+  writeUserData(req.body);
+  res.status(201).send('user added!')
+})
+
+app.post('/createDriver', (req, res) => {
+  writeDriverData(req.body);
+  res.status(201).send('driver added!');
 });
 
 app.post('/createTruck', (req, res) => {
@@ -43,47 +47,60 @@ app.post('/createTruck', (req, res) => {
   res.status(201).send('truck added!');
 });
 
-app.post('/createDriver', (req, res) => {
-  writeDriverData(req.body);
-  res.status(201).send('driver added!');
-});
-app.post('/createUser', (req, res) => {
-  writeUserData(req.body);
-  res.status(201).send('user added!')
-})
-
-app.get('/drivers', (req, res) => {
+app.get('/getUsers', async ( req, res ) => {
   const timestamp = Date.now();
-  getData(drivers);
-  res.status(200).send(`Data recibed correctly from FireBase at ${timestamp}`);
-})
-app.get('/trucks', (req, res) => {
-  const timestamp = Date.now();
-  getData(trucks);
-  res.status(200).send(`Data recibed correctly from FireBase at ${timestamp}`);
-})
-
-app.get('/users', (req, res) => {
-  const timestamp = Date.now();
-  getData(users);
-  res.status(200).send(`Data recibed correctly from FireBase at ${timestamp}`);
+  const users = await getData('users');
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(users);
 }) 
-function getData(val) {
-  return firebase.database.ref(`/${val}`).once('value').then(function(snapshot) {
-    return snapshot.val();
-  })
+
+app.get('/getDrivers', async (req, res) => {
+  const timestamp = Date.now();
+  const drivers = await getData('drivers');
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(drivers);
+})
+
+app.get('/getTrucks', async ( req, res ) => {
+  const timestamp = Date.now();
+  const trucks  = await getData('trucks');
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(trucks);
+})
+
+app.get('/getUserById/:id', async( req, res) => {
+  const timestamp = Date.now();
+  const user = await getData('users/' + req.params.id);
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(user);
+})
+
+app.get('/getDriverById/:id', async (req, res) => {
+  const timestamp = Date.now();
+  const driver = await getData('drivers/'+ req.params.id);
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(driver);
+})
+
+app.get('/getTruckById/:id', async (req, res) => {
+  const timestamp = Date.now();
+  const truck = await getData('trucks/'+ req.params.id);
+  console.log(`Data recibed correctly from FireBase at ${timestamp}`);
+  res.status(200).send(truck);
+})
+
+async function getData(val) {
+  const snapshot = await firebase.database().ref(`/${val}`).once('value');
+  return snapshot.val();
 }
 
-function writeTruckData(truck) {
+function writeUserData(user) {
   const timestamp = Date.now();
-  firebase.database().ref('trucks/' + truck.truckId).set({
-    licensePlate: truck.licensePlate,
-    model: truck.model,
-    brand: truck.brand,
-    year: truck.year
+  firebase.database().ref('users/' + user.id).set({
+    username: user.name,
+    email: user.email,
   });
-
-  console.log(`Data sent to FireBase correctly at ${timestamp}`);
+  console.log(`Data sent to FireBase correctly at ${timestamp}`);  
 }
 
 function writeDriverData(driver) {
@@ -94,15 +111,16 @@ function writeDriverData(driver) {
     document: driver.document,
     address: driver.address
   });
-
   console.log(`Data sent to FireBase correctly at ${timestamp}`);
 }
-function writeUserData(user) {
+
+function writeTruckData(truck) {
   const timestamp = Date.now();
-  firebase.database().ref('users/' + user.id).set({
-    username: user.name,
-    email: user.email,
+  firebase.database().ref('trucks/' + truck.truckId).set({
+    licensePlate: truck.licensePlate,
+    model: truck.model,
+    brand: truck.brand,
+    year: truck.year
   });
   console.log(`Data sent to FireBase correctly at ${timestamp}`);
-  
 }
