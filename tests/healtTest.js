23f03a10194b75684d53f5c479f290b7b6f75b4e
davidovich.marcos <davidovich.marcos@gmail.com>
2020-01-29 10:57:43 -0300

    more specific health test

--- tests/healtTest.js
+++ tests/healtTest.js
@@ -1,21 +1,20 @@
-// Import the dependencies for testing
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../main');
-// Configure chai
 chai.use(chaiHttp);
 chai.should();
+
 describe("Health", () => {
     describe("GET /", () => {
         it("should get 200 ok response", (done) => {
-             chai.request(app)
-                 .get('/')
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                     done();
-                  });
-         });
-       
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.be.equal('Welcome to a transfrio api')
+                    done();
+                });
+        });
+
     });
 });
